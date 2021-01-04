b946dd0dbeadc8f283494bb8a04bb83af1a7ebf4
han-chan-hee <hch99000@gmail.com>
2021-01-05 02:55:49 +0900

    movie app

---
+++ movie_app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+
+
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: []
+    };
+    getmovies = async () => {
+        const {
+            data:
+                {
+                    data:
+                        {movies}
+                }
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+        this.setState({movies, isLoading: false})
+
+    }
+
+     componentDidMount() {
+         this.getmovies();
+    }
+
+    render() {
+        const {isLoading, movies} = this.state;
+        return <div>{isLoading ? "Loading" : movies.map(movie => {
+
+            return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster}/>
+        })}</div>
+    }
+
+}
+
+export default App;
